f56d78f33cbae2dce1d4ed6340b9b38620c8db43
zach323 <30264709+zach323@users.noreply.github.com>
2018-01-05 10:30:07 -0600

    added user input

--- Program.cs
+++ Program.cs
@@ -11,50 +11,59 @@ namespace CSharp_XMLFinder
     {
         static void Main(string[] args)
         {
-            StreamReader reader = new StreamReader("C:\\Users\\User\\Documents\\python4inf.txt");
+            StreamReader reader = new StreamReader("C:\\Users\\zrebstock\\Documents\\python4inf.txt");
             string sLine = "";
             int counter = 0;
-            string target = "<meterNo>";
+            string target;
             string found_string = "";
             int next_index = 0;
             int line_number = 0;
+            Console.WriteLine("Enter string you want to find");
+            target = Console.ReadLine();
             while (sLine != null)
             {
-                sLine = reader.ReadLine();
+                sLine = reader.ReadLine(); //read line for every loop iteration
 
                 foreach (char letter in sLine)
                 {
+                    //loop through chars on each line
 
 
-
-                    if (letter == target[next_index])
+                    if (letter == target[next_index]) //compare char on line to char in target string
                     {
-                        found_string += letter;
-                        next_index++;
-                        Console.WriteLine(found_string);
-                        if (found_string == target)
+                        found_string += letter; //if a match add char in line to found_string placeholder
+                        next_index++; //move index to next index in target string if match was found on prior index
+                        Console.WriteLine(found_string); //output found index for visual purposes (not needed)
+
+                        if (found_string == target) //if target string is located print results, line number and break loop
                         {
                             Console.WriteLine("Found string " + found_string + " on row " + line_number);
                             break;
                         }
 
                     }
-                    else
+                    else //else reset index back to beginning and string placeholder to empty string
                     {
                         next_index = 0;
                         found_string = "";
-                        continue;
+                       // continue;
                     }
 
                 }
-                if (found_string == target)
+                if (found_string == target) //break out of while loop for each line
                 {
                     break;
                 }
                 else
                 {
-                    line_number++;
-                    counter++;
+                    line_number++; //increase line number count and
+                   
+                }
+            
+            if (sLine == null && found_string != target)
+                {
+                    Console.WriteLine("Sorry, could not locate string in file"); //print if string could not be located
+                    break;
                 }
 
             }
