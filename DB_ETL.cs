df367fa7d5ec2e6b97519c352a1ae7ce8d63680f
Zach Rebstock <30264709+zach323@users.noreply.github.com>
2019-03-08 14:01:59 -0600

    Update DB_ETL.cs
    added comments

--- DB_ETL.cs
+++ DB_ETL.cs
@@ -7,6 +7,7 @@ namespace Sensus_DB_ETL
     {
         static void Main(string[] args)
         {
+            //create SQL connection
             SqlConnection myConnection = new SqlConnection("user id=userid;" +
                                        "password=pwd;server=server;" +
                                        "database=db; " +
@@ -24,16 +25,23 @@ namespace Sensus_DB_ETL
 
             try
             {
+                //streamwriter for writing to a csv
                 StreamWriter streamWriter = new StreamWriter("test.csv");
 
-
+                //SqlDataReader to read table rows
                 SqlDataReader myReader = null;
+                //SQL Command to execute SQL queries
+                
                 SqlCommand myCommand = new SqlCommand("SELECT TOP(100) device_id, device_classification_id, modified_date FROM [FlexNetDB].[dbo].[device]", myConnection);
+                //execute sql query
                 myReader = myCommand.ExecuteReader();
+                
+                //get column names
                 Console.Write(myReader.GetName(0));
                 Console.Write(myReader.GetName(1));
                 Console.Write(myReader.GetName(2));
                 Console.WriteLine();
+                //loop through all rows of query and write to csv file with comma added to each element
                 while (myReader.Read())
                 {
                     streamWriter.Write(myReader["device_id"].ToString()+ ",");
@@ -48,6 +56,7 @@ namespace Sensus_DB_ETL
                     Console.WriteLine();
 
                 }
+                //close resources
                 streamWriter.Close();
                 myReader.Close();
 
